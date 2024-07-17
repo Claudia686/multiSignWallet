a81e588549875f9fd9df608525b4f1c1be64f0ca
Claudia686 <claudia_mihaela@rocketmail.com>
2024-07-17 05:04:43 +0100

    Added contract structure

--- contracts/multiSigWallet.sol
+++ contracts/multiSigWallet.sol
@@ -1,4 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
+contract multiSigWallet {
+	address[3] public signatories;
+	uint public requiredSignatures = 2;
 
+ struct Transaction {
+ 	address to;
+ 	uint sentAmount;
+ 	bool executed;
+ 	uint approvalCount;
+}
+
+Transaction[] public transactions;
+
+modifier onlySignatory() {
+	require(isSignatory(msg.sender), 'Not a signatory');
+	_;
+}
+
+ constructor(address[3] memory _signatories) {
+ 	signatories = _signatories;
+ }
+}
